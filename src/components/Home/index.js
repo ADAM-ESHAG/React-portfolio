611266b50ff5a27c895ed42e9e17e2d400e6181a
ADAM ESHAG <adameshag93@gmail.com>
2022-12-30 11:57:49 +0100

    Add About page with animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 
 const Home = () => {
 
@@ -18,27 +19,30 @@ const Home = () => {
     }, []);
 
     return (
-        <div className='container home-page'>
-            <div className='text-zone'>
-                <h1>
-                    <span className='letterClass'>H</span>
-                    <span className={`${letterClass} _12`}>E</span>
-                    <span className={`${letterClass} _13`}>L</span>
-                    <span className={`${letterClass} _14`}>L</span>
-                    <span className={`${letterClass} _15`}>O,</span>
-                    <br />
-                    <span className={`${letterClass} _16`}>I</span>
-                    <span className={`${letterClass} _17`}>'m</span>
-                    <img src={LogoTitle} alt='developer' />
-                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={18}/>
-                    <br />
-                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26}/>
-                </h1>
-                <h2>Front-End Developer / JavaScript / React / NodeJs / </h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+        <>
+            <div className='container home-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <span className={`${letterClass} _11`}>H</span>
+                        <span className={`${letterClass} _12`}>E</span>
+                        <span className={`${letterClass} _13`}>L</span>
+                        <span className={`${letterClass} _14`}>L</span>
+                        <span className={`${letterClass} _15`}>O,</span>
+                        <br />
+                        <span className={`${letterClass} _16`}>I</span>
+                        <span className={`${letterClass} _17`}>'m</span>
+                        <img src={LogoTitle} alt='developer' />
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={18}/>
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26}/>
+                    </h1>
+                    <h2>Front-End Developer / JavaScript / React / NodeJs / </h2>
+                    <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type='pacman'/>
+        </>
     )
 }
 
